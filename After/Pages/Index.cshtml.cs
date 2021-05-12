a4da71eba1c36a1a53be5d1c805651d49fd36ed6
Jared Goodwin <translucency@outlook.com>
2021-05-11 21:55:36 -0700

    Force sign out if user doesn't exist.  Happens with in-memory db when the server restarts.

--- After/Pages/Index.cshtml.cs
+++ After/Pages/Index.cshtml.cs
@@ -14,13 +14,26 @@ namespace After.Pages
     public class IndexModel : PageModel
     {
         private DataService DataService { get; set; }
+        private UserManager<AfterUser> UserManager { get; set; }
         private SignInManager<AfterUser> SignInManager { get; set; }
-        public IndexModel(DataService dataService)
+
+        public IndexModel(DataService dataService, UserManager<AfterUser> userManager, SignInManager<AfterUser> signInManager)
         {
             DataService = dataService;
+            UserManager = userManager;
+            SignInManager = signInManager;
         }
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (await UserManager.GetUserAsync(User) is null)
+                {
+                    await SignInManager.SignOutAsync();
+                    return LocalRedirect("/");
+                }
+            }
+            return Page();
         }
 
         public IActionResult OnPost(string characterName)
